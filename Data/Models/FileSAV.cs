73060c877e407fec5a892c1cff15e817221fecfd
Thando Moeketsi Ndlovu <thandomndlovu@gmail.com>
2025-04-09 01:44:44 +0200

    More data added

---
+++ Data/Models/FileSAV.cs
@@ -0,0 +1,22 @@
+﻿using Database.Afrobarometer.Data.Enums;
+
+using Spss;
+
+using System;
+using System.IO;
+
+namespace Database.Afrobarometer.Data.Models
+{
+	public class FileSAV : SpssReader
+	{
+		public FileSAV(Stream fileStream) : base(fileStream) { }
+		public FileSAV(Stream fileStream, string filename) : this(fileStream) 
+		{
+			Country = default(Countries).FromFilename(filename) ?? throw new ArgumentException();
+			Round = default(Rounds).FromFilename(filename) ?? throw new ArgumentException();
+		}
+
+		public Countries Country { get; set; }
+		public Rounds Round { get; set; }
+	}
+}
